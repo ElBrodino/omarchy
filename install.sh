5693e889f03ddc2fab6e1db149e05de94faa1cfb
David Heinemeier Hansson <david@hey.com>
2025-08-31 15:41:32 +0200

    Extract the reboot segment into its own file

--- install.sh
+++ install.sh
@@ -57,15 +57,4 @@ source $OMARCHY_INSTALL/login/alt-bootloaders.sh
 source $OMARCHY_INSTALL/post-install.sh
 
 # Reboot
-clear
-tte -i ~/.local/share/omarchy/logo.txt --frame-rate 920 laseretch
-echo
-echo "You're done! So we're ready to reboot now..." | tte --frame-rate 640 wipe
-
-if sudo test -f /etc/sudoers.d/99-omarchy-installer; then
-  sudo rm -f /etc/sudoers.d/99-omarchy-installer &>/dev/null
-  echo -e "\nRemember to remove USB installer!\n\n"
-fi
-
-sleep 5
-reboot
+source $OMARCHY_INSTALL/reboot.sh
